735df863b7c338d0910b7fda0b209b30fd393819
Dan Sheffner <Dan@Sheffner.com>
2020-04-11 23:37:12 -0500

    first example of a script that will bootstrap chromebooks with dev tools

--- bootstrap.py
+++ bootstrap.py
@@ -26,10 +26,52 @@
 """
 This program will bootstrap the debian container on chromebooks
 """
+# TODO
+# look for nodejs installation
+# lets start configuring vim
+# what other tools should I install? database tools?
+
 import os
 import sys
-import subprocess
+import subprocess as sb
+import shutil
 import time
+
+# easily change vars
+
+# apt-get packages
+PACKAGES = ["ca-certificates", "build-essential", "htop", "vim", "wget", "tmux"]
+
+# ruby gems
+GEMS = "bundler jekyll"
+
+# golang url
+GOLANG = "https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz"
+GOLANGFILE = GOLANG.split("/")[-1]
+
+def check_return_status(result, message):
+    try:
+        if result.returncode != 0:
+            print (message)
+            sys.exit(1)
+    except AttributeError:
+        print ("could not find returncode value")
+        sys.exit(1)
+
+def add_to_file(filename, contents):
+    f1 = open(filename, 'r')
+    data = f1.read()
+    f1.close()
+
+    if os.path.isfile(filename):
+        if contents not in data:
+            with open(filename, 'a') as outfile:
+                outfile.write(contents + "\n")
+        else:
+            print ("contents already in file skipping. file: " + filename + " contents: " + contents) 
+    else:
+        with open(filename, 'w') as outfile:
+            outfile.write(contents + "\n")
 
 def check_for_root():
 	if os.geteuid() != 0:
@@ -38,24 +80,73 @@ def check_for_root():
 		sys.exit(1)
 
 def apt_get_packages():
-    packages = ["ca-certificates", "build-essential", "htop", "vim", "tmux"]
-    subprocess.run(["apt-get", "update"])
-    install_command = ["apt-get", "-y", "install"] + packages
-    subprocess.run(install_command)
+    result = sb.run(["apt-get", "update"])
+    check_return_status(result, "could not run apt-get update")
+
+    install_command = ["apt-get", "-y", "install"] + PACKAGES
+    result = sb.run(install_command)
+    check_return_status(result, "could not apt-get install packages")
 
 def install_ruby_rails():
     command1 = "gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
     command2 = "curl -sSL https://get.rvm.io | bash -s stable --rails"
-    subprocess.run(command1, shell=True)
-    subprocess.run(command2, shell=True)
+    command3 = "sudo ln -s /usr/local/rvm/rubies/ruby*/bin/ruby /usr/bin/ruby"
+    command4 = "/usr/local/rvm/rubies/ruby*/bin/gem install " + GEMS 
+
+    result = sb.run(command1, shell=True)
+    check_return_status(result, "could not run gpg install for rvm")
+
+    result = sb.run(command2, shell=True)
+    check_return_status(result, "could not install ruby or rails")
+
+    if os.path.isfile("/usr/bin/ruby"):
+        os.remove("/usr/bin/ruby")
+    result = sb.run(command3, shell=True)
+    check_return_status(result, "could not make ruby symlink")
+
+    result = sb.run(command4, shell=True)
+    check_return_status(result, "could not install custom gems")
+
+def install_rust():
+    command1 = "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y"
+    result = sb.call(command1, shell=True)
+
+    # this returns a weird status code for now I'm not going to check this
+    # check_return_status(result, "could not install rust with rustup")
+
+def install_golang():
+    # clean up if we alrady installed it
+    if os.path.isfile(GOLANGFILE):
+        os.remove(GOLANGFILE)
+
+    if os.path.isdir("/usr/local/go"):
+        shutil.rmtree("/usr/local/go")
+
+    command1 = ["wget", GOLANG]
+    command2 = "tar -C /usr/local -xzf go*.tar.gz"
+
+    result = sb.call(command1)
+    result = sb.call(command2, shell=True)
+
+    s1 = "export PATH=$PATH:/usr/local/go/bin"
+    add_to_file('/root/.bashrc', s1)
+
+    d = '/home/'
+    subdirs = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
+
+    for each in subdirs:
+        add_to_file(each + "/.bashrc", s1)
 
 if __name__ == "__main__":
     start = time.time()
+    print ("bootstrap.py started...")
     
     check_for_root()
-    apt_get_packages()
-    install_ruby_rails()
+    # apt_get_packages()
+    # install_ruby_rails()
+    # install_rust()
+    install_golang()
 
     done = time.time()
     elapsed = done - start
-    print ("Time elapsed in seconds: %s" % (elapsed))
+    print ("bootstrap.py completed. Time elapsed in seconds: %s" % (elapsed))
