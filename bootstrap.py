6b9b48412c21acd8654d152e6bf1f0a1e972a9e8
Dan Sheffner <Dan@Sheffner.com>
2020-04-12 20:01:09 -0500

    trying this for vscode

--- bootstrap.py
+++ bootstrap.py
@@ -108,15 +108,15 @@ def set_git_info():
      
 def apt_get_packages():
     # vscode stuff
-    command1 = "curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg"
-    command2 = "mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg"
+    command1 = "curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg"
+    command2 = "install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/"
 
     vscodestring = "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main"
     add_to_file('/etc/apt/sources.list.d/vscode.list', vscodestring)
 
     sb.run(command1, shell=True, check=True)
     sb.run(command2, shell=True, check=True)
-
+    
     sb.run(["apt-get", "update"], check=True)
 
     packages = " ".join(PACKAGES_LIST)
